315bd35efff88aaf0d276c4d514729782a6c5890
lantemir <temiros@mail.ru>
2025-07-18 13:09:02 +0500

    CSRF_TRUSTED_ORIGINS2

--- backend/settings.py
+++ backend/settings.py
@@ -50,9 +50,8 @@ CORS_ALLOW_CREDENTIALS = True  # ✅ Разрешаем куки и автори
 CSRF_COOKIE_SECURE = os.getenv("CSRF_COOKIE_SECURE", "False") == "True"  # ❌ Отключаем secure cookies для локальной разработки
 SESSION_COOKIE_SECURE = os.getenv("SESSION_COOKIE_SECURE", "False") == "True" # ❌ Отключаем secure cookies для локальной разработки
 
-CSRF_TRUSTED_ORIGINS = [
-    origin for origin in os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",") if origin.strip()
-]
+print("CSRF_TRUSTED_ORIGINS =", os.getenv("CSRF_TRUSTED_ORIGINS", "").split(","))
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
 # Application definition
 
 INSTALLED_APPS = [   
